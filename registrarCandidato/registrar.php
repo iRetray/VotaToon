e9aca1359dde9a6802d76b212e11e3650c650c4e
Julian Camilo <julian20141016@gmail.com>
2019-11-17 12:49:54 -0500

    Update registrar.php

--- registrarCandidato/registrar.php
+++ registrarCandidato/registrar.php
@@ -12,9 +12,35 @@
 			<td id="error">
 				<p id="titulos">Formulario de Inscripcion</p>
 				<img src="../imagenes/perfil.png" id="header">
-				<p id="textos">Como candidato, debes registrar tu perfil para que puedas ser elegido el dia de las votaciones. Completa todos los campos a continuacion:</p>
-				
-				<p id="textos"><br><a href="../index.html">Volver al inicio</a></p>
+				<p id="textos">Como candidatos, deben registrar sus perfiles correspondientes (niño y niña) para que puedan ser elegidos el dia de las votaciones. Completa todos los campos a continuacion:</p>
+			</td>
+		</tr>
+		<form method="post" action="">
+		<tr id="general" align="center">
+			<td id="error">
+				<p id="textos">Datos del Niño candidato</p>				
+				<input type="text" name="nombre1" id="texto" placeholder="Nombre Completo">
+				<input type="email" name="correo1" id="texto" placeholder="Correo electronico">
+				<input type="text" name="descripcion1" id="texto" placeholder="Descripcion del candidato">
+				<p id="textos">Foto:</p>	
+				<input type="file" accept="image/*" name="foto1" id="texto" placeholder="Foto del candidato">
+			</td>
+		</tr>
+		<tr id="general" align="center">
+			<td id="error">
+				<p id="textos">Datos de la Niña candidata</p>				
+				<input type="text" name="nombre2" id="texto" placeholder="Nombre Completo">
+				<input type="email" name="correo2" id="texto" placeholder="Correo electronico">
+				<input type="text" name="descripcion2" id="texto" placeholder="Descripcion del candidato">
+				<p id="textos">Foto:</p>	
+				<input type="file" accept="image/*" name="foto2" id="texto" placeholder="Foto del candidato">
+				<input type="submit" name="" id="submit" value="Completar Registro">
+			</td>
+		</tr>
+		</form>
+		<tr>
+			<td id="error">
+				<p id="textos" align="center"><br><a href="../index.html">Volver al inicio</a></p>
 			</td>
 		</tr>	
 	</table>
