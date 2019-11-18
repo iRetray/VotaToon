3db2103bf16bec96f82ef1c20178c2921185fc21
Julian Camilo <julian20141016@gmail.com>
2019-11-18 10:15:57 -0500

    Error al ver candidato

---
+++ verCandidato/candidatoPrueba.php
@@ -0,0 +1,35 @@
+<?php
+include('../conexion.php');
+// Verificar que haya una votacion activa
+/*
+$consulta = "SELECT * FROM `votaciones` WHERE 1";
+$resultado = mysqli_query($conexion, $consulta);
+$hayVotacionActiva = false;
+while ($columna = mysqli_fetch_array( $resultado ))
+{
+	if ($columna['nombreColegio']!="") {
+		$hayVotacionActiva = true;
+	} 
+}
+if ($hayVotacionActiva) {
+	header("Location:../errorNoVotacionActiva.php");
+}
+else{
+	header("Location:../registrar.php");
+}
+*/
+
+header("Location:errorCandidato.php");
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8"/>
+	<title>VotaToon</title>
+	<link rel="stylesheet" type="text/css" href="estilos.css">
+</head>
+<body class="bodyStyle">
+
+</body>
+</html>
