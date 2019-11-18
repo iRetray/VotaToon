f9c2a0333a7033d1c9086530c39b7b78852bba94
Julian Camilo <julian20141016@gmail.com>
2019-11-18 10:09:46 -0500

    Ver candidato iniciado

---
+++ verCandidato/verCandidato.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8"/>
+	<title>VotaToon</title>
+	<link rel="stylesheet" type="text/css" href="../estilos.css">
+</head>
+<body class="bodyStyle">
+	<center>
+	<table id="estiloSombra" align="center">
+		<tr id="general" align="center">
+			<td id="error">
+				<p id="titulos">No hay votaciones activas</p>
+				<img src="../imagenes/error.png" id="header">
+				<p id="textos">No hay ninguna votacion activa para que puedas registrar tu perfil de personero. Comunicate con el administrador de tu institucion para que cree una votacion.<br><a href="../index.html">Volver al inicio</a></p>
+			</td>
+		</tr>	
+	</table>
+	</center>
+</body>
+</html>
