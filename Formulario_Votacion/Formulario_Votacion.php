c287a53593cfc616d6e80856445eb0a7c179c4ab
eovallea3786 <eovallea3786@universidadean.edu.co>
2019-11-19 17:18:14 -0500

    Update Formulario_Votacion.php

--- Formulario_Votacion/Formulario_Votacion.php
+++ Formulario_Votacion/Formulario_Votacion.php
@@ -2,10 +2,8 @@
 include('../conexion.php');
 		
 	$conexion = mysqli_connect($servidor, $usuarioDB, $contraseña, $baseDeDatos)or die ("Error al conectar al servidor"); 
-
 	$db = mysqli_select_db($conexion, $baseDeDatos)	or die ("Eror al conectar a la BD");
-
-	$consulta = "SELECT * FROM `candidatos` WHERE 1";
+	$consulta = "SELECT * FROM `resultados` WHERE 1";
 	$resultado = mysqli_query($conexion, $consulta);
 	$columna = mysqli_fetch_array( $resultado );
 
