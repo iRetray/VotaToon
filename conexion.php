09e30efb8854458b28b22d9a861625ce74666b68
eovallea3786 <eovallea3786@universidadean.edu.co>
2019-11-20 11:27:48 -0500

    Update conexion.php
    contraseña root

--- conexion.php
+++ conexion.php
@@ -1,7 +1,7 @@
 <?php
 	$servidor = "localhost";
 	$usuarioBD = "root";
-	$contraseña = "12345678";
+	$contraseña = "";
 	$baseDeDatos = "votaciones";
 	$conexion = mysqli_connect($servidor, $usuarioBD, $contraseña, $baseDeDatos) or die ("La conexion al servidor no fue establecida");
 	$db = mysqli_select_db($conexion,$baseDeDatos) or die ("No se logro conectar a la base de datos");
