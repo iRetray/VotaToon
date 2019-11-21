96a688755938395f5f20edc7f45f36e59afcb14c
JoseSissa <53451389+JoseSissa@users.noreply.github.com>
2019-11-21 17:48:01 -0500

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,7 +1,7 @@
 <?php
 	$servidor = "localhost";
 	$usuarioBD = "root";
-	$contraseña = "";
+	$contraseña = "12345678";
 	$baseDeDatos = "votaciones";
 	$conexion = mysqli_connect($servidor, $usuarioBD, $contraseña, $baseDeDatos) or die ("La conexion al servidor no fue establecida"); // Check connection al servidor
 	$db = mysqli_select_db($conexion,$baseDeDatos) or die ("No se logro conectar a la base de datos"); // Check connection a la base de datos
