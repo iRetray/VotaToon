7b35ac8ccb2e909be1b3d7dfd40467e06f0b25f7
JoseSissa <53451389+JoseSissa@users.noreply.github.com>
2019-11-21 15:07:21 -0500

    Update conex.php

--- ConsultarResultados/conex.php
+++ ConsultarResultados/conex.php
@@ -1,10 +1,10 @@
 <?php
-
-$mysqli = new mysqli("localhost","root","12345678","votaciones");
-
+	$servidor = "localhost";
+	$usuarioBD = "root";
+	$contraseña = "12345678";
+	$baseDeDatos = "votaciones";
+	$conexion = mysqli_connect($servidor, $usuarioBD, $contraseña, $baseDeDatos) or die ("La conexion al servidor no fue establecida");
+	$db = mysqli_select_db($conexion,$baseDeDatos) or die ("No se logro conectar a la base de datos");
 ?>
-
-
-
 
 
